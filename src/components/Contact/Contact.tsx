d955dad17b582afa94f4c50bf0b4d28ae6f6fb66
kmerbcn <62482095+kmerbcn@users.noreply.github.com>
2026-07-22 17:32:52 +0200

    v0.4

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,6 +4,7 @@ import './Contact.css';
 export function Contact() {
     return(
         <section id="contact" className="contact-section">
+            <h2>Contact</h2>    
             <a href={`mailto:${portfolio.profile.contact}`} className="contact-link">
                 {portfolio.profile.contact}  
             </a>
